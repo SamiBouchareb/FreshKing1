ad80e5df1a5e9beb3f80711f43b7620ccb81b286
SamiBouchareb <Boucharebismail@gmail.com>
2024-12-05 22:35:52 +0100

    update: add new API key to config

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 // Note: This is not the recommended way to store API keys
 // This is only for development/testing purposes
-export const CHATBOT_API_KEY: string = "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";
+export const CHATBOT_API_KEY: string = "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";
